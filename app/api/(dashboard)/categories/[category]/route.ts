5f4fd98b2a5fd1b5e4e8294a581c9fa303bc505d
unknown <umairahmedimranbutt@gmail.com>
2024-07-27 15:18:03 +0500

    Adding DELETE operation to categories

--- app/api/(dashboard)/categories/[category]/route.ts
+++ app/api/(dashboard)/categories/[category]/route.ts
@@ -55,3 +55,46 @@ export const PATCH = async (request: Request, context: { params: any }) => {
     });
   }
 };
+
+export const DELETE = async (request: Request, context: { params: any }) => {
+  const categoryId = context.params.category;
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse("Invalid or missing User ID", { status: 400 });
+    }
+    if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
+      return new NextResponse("Invalid or missing Category ID", {
+        status: 400,
+      });
+    }
+    await connect();
+    const user = await User.findById(userId);
+    if (!user) {
+      return new NextResponse("User not found", { status: 404 });
+    }
+    const category = await Category.findOne({ _id: categoryId, user: userId });
+    if (!category) {
+      return new NextResponse(
+        "Category not found or does not belong to the user",
+        { status: 404 }
+      );
+    }
+
+    await Category.findByIdAndDelete(categoryId);
+
+    return new NextResponse(
+      JSON.stringify({
+        message: "Category is deleted",
+      }),
+      {
+        status: 200,
+      }
+    );
+  } catch (error: any) {
+    return new NextResponse("Error in deleting category" + error.message, {
+      status: 500,
+    });
+  }
+};
